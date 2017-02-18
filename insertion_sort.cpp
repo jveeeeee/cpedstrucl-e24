f77b2f3ddf2fe099e184b6bd202e896762a2334f
John Vincent Evangelista <evangelistajohn015@gmail.com>
2017-02-18 11:58:43 +0800

    Create insertion_sort.cpp

---
+++ insertion_sort.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int n=5,x=1,j;
+	int A[] = {40,30,20,50,10};
+	
+	for (int i=1;i<n;i++){
+		int tmp=A[i];
+		for( j=i-1;j>=0 && tmp<A[j];j--){
+			A[j+1] = A[j];
+		}	
+		A[j+1] = tmp;
+		cout << "Pass#" << x++ << ": ";		
+		for (int z=0;z<n;z++){
+			cout << A[z] << " ";
+		}	
+		cout << "Swapped: " << A[i] << "," << A[i+1];
+		cout << endl;
+		}
+}
+	
