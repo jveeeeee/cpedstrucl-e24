bfef8d78c1198094e9086851398459d6f28d2eff
John Vincent Evangelista <evangelistajohn015@gmail.com>
2017-02-18 11:57:56 +0800

    Update selection_sort.cpp

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
 	int n=5,x=1;
-	int A[] = {35,10,55,20,5};
+	int A[] = {30, 40, 20, 50, 10};
 	for (int i=0;i<n-1;i++){
 		int min=A[i], index = i;
 		for (int j=i+1; j<n;j++){
@@ -14,10 +14,11 @@ int main(){
 		}
 	A[index] = A[i];
 	A[i] = min;
-	cout << "Pass#" << x++ << " ";		
+	cout << "Pass#" << x++ << ": ";		
 	for (int z=0;z<n;z++){
 	cout << A[z] << " ";
-	}	
-	cout << endl;
 	}
+	cout << "Swapped: " << A[i] << "," << A[i+1];
+	cout << endl;
+	}	
 }
