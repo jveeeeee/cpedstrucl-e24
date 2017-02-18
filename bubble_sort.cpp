1ef04fdc9a96523e4e0a2c8cffe21c20eb9dda24
John Vincent Evangelista <evangelistajohn015@gmail.com>
2017-02-18 12:37:21 +0800

    Update bubble_sort.cpp

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-	int n=5,x=1;;
+	int n=5,x=1,j;;
 	int A[] = {35,10,55,20,5};
 	
 	
@@ -12,13 +12,18 @@ int main(){
 				int swap = A[j];
 				A[j] = A[j+1];
 				A[j+1] = swap;
+				for (int k=0;k<1;k++){
 				cout << "Pass#" << x++ << ": ";
 					for (int z=0;z<n;z++){
 						cout << A[z] << " ";
 					}	
 					cout << "Swapped: " << A[j] << "," << A[j+1];
-					cout << endl;
-			}		
+					cout << endl;		
 		}
-	}	
-}
+				}
+				
+			}
+		}
+		
+	}
+
